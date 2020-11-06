bb5f4c2182033fb42b68e15e6c66e8dadccda277
Your Name <you@example.com>
2020-11-06 21:39:54 +0530

    Modified: some errors

--- src/main/java/com/blz/hashtables/MyHashMap.java
+++ src/main/java/com/blz/hashtables/MyHashMap.java
@@ -1,9 +1,9 @@
 package com.blz.hashtables;
 
 public class MyHashMap<K, V> {
-	
+
 	MyLinkedList<K> myLinkedList;
-	
+
 	public MyHashMap() {
 		this.myLinkedList = new MyLinkedList<>();
 	}
@@ -15,14 +15,14 @@ public class MyHashMap<K, V> {
 
 	public void add(K key, V value) {
 		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
-		if(myMapNode == null) {
+		if (myMapNode == null) {
 			myMapNode = new MyMapNode<>(key, value);
 			this.myLinkedList.append(myMapNode);
-		}else {
+		} else {
 			myMapNode.setValue(value);
 		}
-	}	
-	
+	}
+
 	@Override
 	public String toString() {
 		return "MyHashMapNodes{" + myLinkedList + '}';
