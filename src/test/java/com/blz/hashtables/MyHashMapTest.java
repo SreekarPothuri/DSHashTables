4a502a2c86cf84e7ac8c08ad9ff878f6b4a031ab
Your Name <you@example.com>
2020-11-06 22:03:58 +0530

    Added: Test case to check remove method

--- src/test/java/com/blz/hashtables/MyHashMapTest.java
+++ src/test/java/com/blz/hashtables/MyHashMapTest.java
@@ -41,4 +41,24 @@ public class MyHashMapTest {
 		System.out.println(myLinkedHashMap);
 		Assert.assertEquals(3, frequency);
 	}
+	
+	@Test
+	public void givenParagraphwhenWordsAreRemovedFromListShouldReturnRemovedWord() {
+		String sentence = "Paranoids are not paranoid because they are paranoid "
+				+ "but because they keep putting themselves " + "deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<String, Integer>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		myLinkedHashMap.remove("avoidable");
+		Integer frequency = myLinkedHashMap.get("avoidable");
+		Assert.assertEquals(null, frequency);
+	}
+
 }
