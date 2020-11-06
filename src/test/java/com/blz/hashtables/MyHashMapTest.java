efe8977e883a0139af6c64f4e1083dc65ac25fa2
Your Name <you@example.com>
2020-11-06 21:47:43 +0530

    Added: Test case to find frequency of word in given paragraph

--- src/test/java/com/blz/hashtables/MyHashMapTest.java
+++ src/test/java/com/blz/hashtables/MyHashMapTest.java
@@ -3,24 +3,42 @@ package com.blz.hashtables;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class MyHashMapTest {
-	
+
 	@Test
 	public void givenASentenceWhenWordsAreAddedToListShouldReturnWordFrequency() {
 		String sentence = "To be or not to be";
 		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
-		for(String word : words) {
+		for (String word : words) {
 			Integer value = myHashMap.get(word);
-			if(value == null)
+			if (value == null)
 				value = 1;
 			else
-				value = value +1;
+				value = value + 1;
 			myHashMap.add(word, value);
 		}
 		int frequency = myHashMap.get("to");
 		System.out.println(myHashMap);
 		Assert.assertEquals(2, frequency);
+	}
+
+	@Test
+	public void givenAParagraphWhenWordsAreAddedToListShouldReturnParanoidFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep puting themselves deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
+		System.out.println(myLinkedHashMap);
+		Assert.assertEquals(3, frequency);
 	}
 }
